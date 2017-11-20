1ac9a803663eb999e6d3e159a826abdba6824a7f
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-20 20:22:33 +0700

    Refactor code

--- Exercise02/Model/IUserService.cs
+++ Exercise02/Model/IUserService.cs
@@ -1,5 +1,6 @@
 ﻿using RestSharp;
 using Retrofit.Net.Attributes.Methods;
+using Retrofit.Net.Attributes.Parameters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,18 @@ namespace Exercise02.Model
     public interface IUserService
     {
         [Get("users")]
-        RestResponse<List<User>> GetUser();
+        RestResponse<List<User>> GetUsers();
+
+        [Get("users/{id}")]
+        RestResponse<User> GetUser([Path("id")] int id);
+
+        [Post("users")]
+        RestResponse<bool> EditUser([Body] object obj);
+
+        [Put("users/{id}")]
+        RestResponse<bool> UpdateUser([Path("id")] int id, [Body] object obj);
+
+        [Delete("users/{id}")]
+        RestResponse<bool> DeleteUser([Path("id")] int id);
     }
 }
