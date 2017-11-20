c85589c11cbaa0e9e066ecd5a05e194baef3b6de
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-20 15:44:16 +0700

    Add Exercise02

---
+++ Exercise02/Model/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+using Retrofit.Net.Attributes.Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.Model
+{
+    public interface IUserService
+    {
+        [Get("users")]
+        RestResponse<List<User>> GetUser();
+    }
+}
---
+++ Exercise02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Retrofit;
+using System.Net.Http;
+using Retrofit.Net;
+using Exercise02.Model;
+using RestSharp;
+
+namespace Exercise02
+{
+    class Program
+    {
+        public static object obj { set; get; }
+
+        static void Main(string[] args)
+        {
+            RestAdapter adapter = new RestAdapter(@"https://jsonplaceholder.typicode.com/");
+            IUserService service = adapter.Create<IUserService>();
+            RestResponse<List<User>> userResponse = service.GetUser();
+            List<User> listUser = userResponse.Data;
+            listUser.ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+    }
+}
