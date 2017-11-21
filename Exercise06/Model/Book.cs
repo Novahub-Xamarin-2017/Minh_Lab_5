fa49dfa2fd217e7aaf58925e2c69ebd2ddaccb85
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-21 12:38:54 +0700

    Add Exercise06

---
+++ Exercise06/Model/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise06.Model
+{
+    [XmlRoot(ElementName = "book")]
+    public class Book
+    {
+        [XmlElement(ElementName = "author")]
+        public string Author { get; set; }
+
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        [XmlElement(ElementName = "genre")]
+        public string Genre { get; set; }
+
+        [XmlElement(ElementName = "price")]
+        public string Price { get; set; }
+
+        [XmlElement(ElementName = "publish_date")]
+        public string Publish_date { get; set; }
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+    }
+}
---
+++ Exercise06/Program.cs
@@ -0,0 +1,69 @@
+﻿using Exercise06.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise06
+{
+    public class Program
+    {
+        public static string filePath = "../../../XmlFile/booksExercise06.xml";
+
+        static void Main(string[] args)
+        {
+            Console.Write("FilePath of XmlFile: ");
+            var str = Console.ReadLine();
+            if (!str.Equals(""))
+            {
+                filePath = str;
+            }
+            var xmlString = ReadFile(filePath);
+
+            var xml = new XmlDocument();
+            xml.LoadXml(xmlString);
+
+            var jsonString = JsonConvert.SerializeXmlNode(xml);
+            var streamwriter = new StreamWriter("../../../JsonFile/booksExercise06.json");
+
+            Console.WriteLine(jsonString);
+            streamwriter.Write(jsonString);
+
+            /*var obj = JsonConvert.DeserializeObject<Catalog>(xmlString);
+            JsonConvert.d
+            var xmlSerializer = new XmlSerializer(obj.GetType());
+
+            var strWriter = new StringWriter();
+            var writer = XmlWriter.Create(strWriter);
+            xmlSerializer.Serialize(writer, obj);
+
+            var xmlString = strWriter.ToString();
+            Console.WriteLine(xmlString);
+
+            var qweqwe = new XmlDocument();
+            qweqwe.LoadXml(xmlString);
+            qweqwe.Save("../../../JsonFile/booksExercise06.json");*/
+
+            Console.ReadKey();
+        }
+
+        static string ReadFile(string filePath)
+        {
+            var sr = new StreamReader(filePath);
+            var str = "";
+            var line = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                str = str + line + "\n";
+            }
+
+            return str;
+        }
+    }
+}
