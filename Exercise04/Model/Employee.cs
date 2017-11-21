d2b7428ca53d166886d949eef2325ed7198472f8
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-21 11:41:35 +0700

    Add Exercise04

---
+++ Exercise04/Model/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04.Model
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember(Name = "userId")]
+        public string UserId { get; set; }
+
+        [DataMember(Name = "jobTitleName")]
+        public string JobTitleName { get; set; }
+
+        [DataMember(Name = "firstName")]
+        public string FirstName { get; set; }
+
+        [DataMember(Name = "lastName")]
+        public string LastName { get; set; }
+
+        [DataMember(Name = "preferredFullName")]
+        public string PreferredFullName { get; set; }
+
+        [DataMember(Name = "employeeCode")]
+        public string EmployeeCode { get; set; }
+
+        [DataMember(Name = "region")]
+        public string Region { get; set; }
+
+        [DataMember(Name = "phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [DataMember(Name = "emailAddress")]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Exercise04/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise04
+{
+    public class Program
+    {
+        public static string filePath = "../../../employees.json";
+
+        static void Main(string[] args)
+        {
+            Console.Write("FilePath of JsonFile: ");
+            var str = Console.ReadLine();
+            if (!str.Equals(""))
+            {
+                filePath = str;
+            }
+            var jsonString = ReadFile(filePath);
+
+            var obj = JsonConvert.DeserializeObject<Employess>(jsonString);
+            var xmlSerializer = new XmlSerializer(obj.GetType());
+
+            var strWriter = new StringWriter();
+            var writer = XmlWriter.Create(strWriter);
+            xmlSerializer.Serialize(writer, obj);
+
+            var xmlString = strWriter.ToString();
+            Console.WriteLine(xmlString);
+
+            var qweqwe = new XmlDocument();
+            qweqwe.LoadXml(xmlString);
+            qweqwe.Save("../../../employees.xml");
+
+            Console.ReadKey();
+        }
+
+        static string ReadFile(string filePath)
+        {
+            var sr = new StreamReader(filePath);
+            var str = "";
+            var line = "";
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                str = str + line + "\n";
+            }
+
+            return str;
+        }
+    }
+}
